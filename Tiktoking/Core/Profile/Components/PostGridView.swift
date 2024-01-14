5a47adc749233d8f0d002e5b4d8a89144a23c7b1
Asep Hikmat Fatahillah <asephikmatf@gmail.com>
2024-01-14 23:49:24 +0700

    create profile header view

---
+++ Tiktoking/Core/Profile/Components/PostGridView.swift
@@ -0,0 +1,8 @@
+//
+//  PostGridView.swift
+//  Tiktoking
+//
+//  Created by Asep Hikmat Fatahillah on 14/01/24.
+//
+
+import Foundation
---
+++ Tiktoking/Core/Profile/CurrentUserProfileView.swift
@@ -0,0 +1,30 @@
+//
+//  CurrentUserProfileView.swift
+//  Tiktoking
+//
+//  Created by Asep Hikmat Fatahillah on 14/01/24.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 2) {
+                    // profile header
+                    ProfileHeaderView()
+                    
+                    // post as grid view
+                }
+                .padding(.top)
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    CurrentUserProfileView()
+}
