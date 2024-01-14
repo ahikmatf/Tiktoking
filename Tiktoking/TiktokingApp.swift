3cd0775591ec1a2f337040be0c9c688830b0ac2e
Asep Hikmat Fatahillah <asephikmatf@gmail.com>
2024-01-14 21:26:09 +0700

    Initial Commit

---
+++ Tiktoking/TiktokingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiktokingApp.swift
+//  Tiktoking
+//
+//  Created by Asep Hikmat Fatahillah on 14/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TiktokingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
